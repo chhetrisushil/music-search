3f8b0ed643d31171b73ca10269e8bbfd95517d37
Chhetri <chhes@amazon.com>
2017-05-18 11:13:44 +0530

    Correcting element name

--- src/components/search-drawer/search-drawer.js
+++ src/components/search-drawer/search-drawer.js
@@ -5,38 +5,39 @@
  * Distributed under terms of the MIT license.
  */
 /*global HTMLElement, templateEngine: true*/
-let template = document.currentScript.ownerDocument.querySelector('#t');
+(function () {
+  let template = document.currentScript.ownerDocument.querySelector('#t');
 
-class MyElement extends HTMLElement {
-  static get observedAttributes() {
-    return ['name'];
-  }
+  class SearchDrawer extends HTMLElement {
+    static get observedAttributes() {
+      return ['name'];
+    }
 
-  constructor() {
-    super();
-    if (window.isSafari) {
-      this.root = this.attachShadow({
-        mode: 'closed'
-      });
-    } else {
-      this.root = this.createShadowRoot();
+    constructor() {
+      super();
+      if (window.isSafari) {
+        this.root = this.attachShadow({
+          mode: 'closed'
+        });
+      } else {
+        this.root = this.createShadowRoot();
+      }
+    }
+
+    attributeChangedCallback(attr, oldValue, newValue) {
+      // let clone = document.importNode(template.content, true);
+      let str = template.innerHTML;
+
+      if (attr === 'name') {
+        str = templateEngine(str, {
+          name: newValue
+        });
+        // clone.querySelector('.test')
+        // .textContent = `Hello, ${newValue}!!!`;
+        this.root.innerHTML = str;
+      }
     }
   }
 
-  attributeChangedCallback(attr, oldValue, newValue) {
-    // let clone = document.importNode(template.content, true);
-    let str = template.innerHTML;
-      console.log(template.outerHTML);
-
-    if (attr === 'name') {
-      str = templateEngine(str, {
-        name: newValue
-      });
-      // clone.querySelector('.test')
-      // .textContent = `Hello, ${newValue}!!!`;
-      this.root.innerHTML = str;
-    }
-  }
-}
-
-window.customElements.define('my-element', MyElement);
+  window.customElements.define('search-drawer', SearchDrawer);
+})();
